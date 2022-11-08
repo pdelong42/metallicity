188cd18b98c2d6c2170d60aff56ea7a75854c122
Paul DeLong <pdelong@amnh.org>
2022-11-07 22:58:48 -0500

    updated some placeholders in Git project;

--- github.com/git/go.sh
+++ github.com/git/go.sh
@@ -16,13 +16,24 @@ EOF
 #  - git.c
 #
 # next steps:
+#  - pager.c
 #  - usage.c
 #  - setup.c
 #
 # todo:
+#  - chdir-notify.c
+#  - chdir-notify.h
+#  - repository.c
+#  - repository.h
 #  - string-list.c
 #  - strvec.c
 #  - trace.c
 #  - trace.h
 #  - trace2.c
 #  - trace2.h
+#  - wrapper.c
+
+# Fix the for-loop in validate_cache_entries(), in the file
+# read-cache.c; see 8616a2d0cb57865540f1c00ac2e5385a6cc5d84e for the
+# full context.  Before attempting to submit the fix, read the
+# guidelines at https://git-scm.com/community
