437e2f3eae25d0674b3656969558ab4e1a7142b0
Paul DeLong <pdelong@amnh.org>
2022-11-28 22:40:21 -0500

    moved placeholders into notes.txt;

--- github.com/git/go.sh
+++ github.com/git/go.sh
@@ -11,27 +11,3 @@ strbuf.h
 string-list.h
 strvec.h
 EOF
-
-# presently on:
-#  - git.c
-#
-# next steps:
-#  - help.c
-#  - pager.c
-#  - usage.c
-#  - setup.c
-#
-# todo:
-#  - chdir-notify.c
-#  - chdir-notify.h
-#  - config.c
-#  - config.h
-#  - repository.c
-#  - repository.h
-#  - string-list.c
-#  - strvec.c
-#  - trace.c
-#  - trace.h
-#  - trace2.c
-#  - trace2.h
-#  - wrapper.c
